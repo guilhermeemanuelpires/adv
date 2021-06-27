52d2d26c1ac6d56ea576765dc71ef04deb5b7d6d
Guilherme Pires <guilhermeemanuel45@gmail.com>
2021-06-27 14:59:14 -0300

    ajustando estrutura do projeto

---
+++ app-adv/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Seja Bem-Vindo ao ADV App, ainda estamos em fase de desenvolvimento, logo mais você será notificado com novas atualizações e novidades...</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  }
+});
